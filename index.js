1c71bdf9581b411f8a8b7f059bb4ec6a998a2073
Leonardo Casales <leonardo@smartnet.com.ar>
2020-07-29 20:37:36 -0300

    cors added

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-"use strict";
+'use strict' ;
 
-const http = require("http");
-const express = require("express");
-const bodyParser = require("body-parser");
-require('dotenv').config()
-const morgan = require('morgan');
-const config = require("./config/config");
+const http = require( 'http' ) ;
+const express = require( 'express' ) ;
+const bodyParser = require( 'body-parser' ) ;
+var cors = require( 'cors' ) ;
+require( 'dotenv' ).config() ;
+const morgan = require( 'morgan' ) ;
+const config = require( './config/config' ) ;
 
-const app = express();
-const router = require("./router.js");
+const app = express() ;
+app.use( cors() ) ;
+const router = require( './router.js' ) ;
 
-app.use(morgan('combined'));
-
-app.use(
-  bodyParser.json({
-    limit: "50mb"
-  })
-);
+app.use( morgan( 'combined' ) ) ;
 
 app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true
-  })
-);
+    bodyParser.json( {
+        limit: '50mb'
+    } )
+) ;
 
-app.use("/", router);
+app.use(
+    bodyParser.urlencoded( {
+        limit: '50mb',
+        extended: true
+    } )
+) ;
 
-http.createServer(app).listen(config.port, () => console.log(`Escuchando en el puerto ${config.port}`));
+app.use( '/', router ) ;
 
-module.exports = app;
+http.createServer( app ).listen( config.port, () => console.log( `Escuchando en el puerto ${config.port}` ) ) ;
+
+module.exports = app ;
